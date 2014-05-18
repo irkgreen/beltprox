5348fccc21d43e6dc2906f66b589e5dfb90c5fc9
unknown <Eric@.(none)>
2014-05-18 17:57:01 -0400

    test commit

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,7 +1,6 @@
 
 /// Copyright 2011, Ian Gilman
 /// Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
-///
 /// <reference path="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.2.js" />
 /// <reference path="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.21/jquery-ui.min.js" />
 /// <reference path="Scripts/jquery.ui.touch-punch.min.js" />
